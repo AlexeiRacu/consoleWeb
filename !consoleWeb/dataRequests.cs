6bb5f035ea094a297ef8e06603a5b3e4f245f5cc
Алексей <alesha3241@gmail.com>
2024-07-23 15:52:04 +0300

    Update dataRequests.cs

--- !consoleWeb/dataRequests.cs
+++ !consoleWeb/dataRequests.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.VisualBasic;
 using Npgsql;
 namespace dataRequests
 {
-    class readInfo
+    class data
     {
         static readonly string conString = "Host=127.0.0.1;Port=5432;Database=postgres;Username=postgres;Password=1510;Timeout=10;SslMode=Prefer";
+
 
         public static string readUsers(int user_id, string chooseInfo = "1111")
         {
@@ -34,12 +34,33 @@ namespace dataRequests
                 {
                     userInfo += $"Root права: {reader["user_root"]}, ";
                 }
+                connect.Close();
                 return userInfo.TrimEnd(' ', ',');
             }
             else
             {
                 return "Несуществующий пользователь!";
             }
+        }
+        public static void createUser(string name, string password, string root = "false")
+        {
+            var connect = new NpgsqlConnection(conString);
+            connect.Open();
+            var cmd = connect.CreateCommand();
+            cmd.CommandText = "INSERT INTO users (user_name, user_password, user_root) VALUES(@name, @password, @root)";
+
+            // Преобразование значения root в логическое значение
+            bool rootBool = root.ToLower() == "true";
+
+            // Добавление параметров запроса
+            cmd.Parameters.AddWithValue("name", name);
+            cmd.Parameters.AddWithValue("password", password);
+            cmd.Parameters.AddWithValue("root", rootBool);
+
+            // Выполнение запроса на вставку данных
+            cmd.ExecuteNonQuery();
+
+            connect.Close();
         }
     }
 }
