0c21cfdc970dd1464b938da8a315389d3ea2cce3
Алексей <alesha3241@gmail.com>
2024-07-24 19:06:03 +0300

    Update dataRequests.cs

--- !consoleWeb/dataRequests.cs
+++ !consoleWeb/dataRequests.cs
@@ -6,7 +6,7 @@ namespace dataRequests
         static readonly string conString = "Host=127.0.0.1;Port=5432;Database=postgres;Username=postgres;Password=1510;Timeout=10;SslMode=Prefer";
 
 
-        public static string readUsers(int user_id, string chooseInfo = "1111")
+        public static string readUsers(int user_id, string chooseInfo = "11111")
         {
             var connect = new NpgsqlConnection(conString);
             connect.Open();
@@ -20,19 +20,23 @@ namespace dataRequests
                 string userInfo = "";
                 if (chooseInfo[0] != '0')
                 {
-                    userInfo += $"ID: {reader["user_id"]}, ";
+                    userInfo += $"{reader["user_id"]}, ";
                 }
                 if (chooseInfo[1] != '0')
                 {
-                    userInfo += $"Имя: {reader["user_name"]}, ";
+                    userInfo += $"{reader["user_login"]}, ";
                 }
                 if (chooseInfo[2] != '0')
                 {
-                    userInfo += $"Пароль: {reader["user_password"]}, ";
+                    userInfo += $"{reader["user_name"]}, ";
                 }
                 if (chooseInfo[3] != '0')
                 {
-                    userInfo += $"Root права: {reader["user_root"]}, ";
+                    userInfo += $"{reader["user_password"]}, ";
+                }
+                if (chooseInfo[4] != '0')
+                {
+                    userInfo += $"{reader["user_root"]}, ";
                 }
                 connect.Close();
                 return userInfo.TrimEnd(' ', ',');
@@ -42,6 +46,48 @@ namespace dataRequests
                 return "Несуществующий пользователь!";
             }
         }
+
+        public static string readUsers(string user_login, string chooseInfo = "11111")
+        {
+            var connect = new NpgsqlConnection(conString);
+            connect.Open();
+            var cmd = connect.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM users WHERE user_login = '{user_login}'";
+
+            var reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string userInfo = "";
+                if (chooseInfo[0] != '0')
+                {
+                    userInfo += $"{reader["user_id"]}, ";
+                }
+                if (chooseInfo[1] != '0')
+                {
+                    userInfo += $"{reader["user_login"]}, ";
+                }
+                if (chooseInfo[2] != '0')
+                {
+                    userInfo += $"{reader["user_name"]}, ";
+                }
+                if (chooseInfo[3] != '0')
+                {
+                    userInfo += $"{reader["user_password"]}, ";
+                }
+                if (chooseInfo[4] != '0')
+                {
+                    userInfo += $"{reader["user_root"]}, ";
+                }
+                connect.Close();
+                return userInfo.TrimEnd(' ', ',');
+            }
+            else
+            {
+                return "Несуществующий пользователь!";
+            }
+        }
+
         public static void createUser(string login, string name, string password, string root = "false")
         {
             var connect = new NpgsqlConnection(conString);
@@ -51,12 +97,13 @@ namespace dataRequests
             cmd.ExecuteNonQuery();
         }
 
+
     }
     class dataChek
     {
         public static bool chekConditions(string user_data, params string[] parameter)
         {
-            bool isCheckPassed = true; // Начальное состояние успешного прохождения проверки
+            bool isCheckPassed = false; // Начальное состояние успешного прохождения проверки
 
             for (int i = 0; i < parameter.Length; i++)
             {
@@ -68,19 +115,32 @@ namespace dataRequests
                     case "maxlength":
                         if (user_data.Length > int.Parse(value))
                         {
-                            isCheckPassed = false; // Условие не выполняется
+                            isCheckPassed = true;
                             return isCheckPassed;
                         }
                         break;
                     case "minlength":
                         if (user_data.Length < int.Parse(value))
                         {
-                            isCheckPassed = false; // Условие не выполняется
+                            isCheckPassed = true;
                             return isCheckPassed;
                         }
                         break;
+                    case "uniq":
+                        switch (value)
+                        {
+                            case "login":
+                                if (user_data == data.readUsers(user_data, "01000"))
+                                {
+                                    isCheckPassed = true;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                        break; // Завершение метки "uniq"
                     default:
-                        return false; // Неизвестное условие
+                        return false;
                 }
             }
 
