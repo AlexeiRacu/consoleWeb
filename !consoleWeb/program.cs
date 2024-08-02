5c14c79892a5df4838f021c7988a4b0d2f96026c
Алексей <alesha3241@gmail.com>
2024-08-02 16:55:01 +0300

    Update program.cs

--- !consoleWeb/program.cs
+++ !consoleWeb/program.cs
@@ -1,16 +1,17 @@
 using allCommands;
+using System.Text;
 using visual;
 
 namespace _consoleWeb
 {
     class program
     {
-        public static void Main() 
+        public static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.ForegroundColor = ConsoleColor.White;
             while (true)
             {
-
                 string request = Console.ReadLine();
                 if (request == "" || request[0] != '/')
                 {
