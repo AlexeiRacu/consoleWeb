a18bfe91c93b48de1e455da04ac1f77e2d7bd31c
Алексей <alesha3241@gmail.com>
2024-08-04 17:09:09 +0300

    Update program.cs

--- !consoleWeb/program.cs
+++ !consoleWeb/program.cs
@@ -6,11 +6,18 @@ namespace _consoleWeb
 {
     class program
     {
+        private static int MainCalls = 0;
+        private static int PostsPageCalls = 0;
         public static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.Unicode;
             Console.ForegroundColor = ConsoleColor.White;
+            if(MainCalls == 0)
+            {
+                customWrite.writeLine("/help для описания работы с программой");
+            }
+            MainCalls++;
             while (true)
             {
                 string request = Console.ReadLine();
@@ -21,6 +28,28 @@ namespace _consoleWeb
                 else
                 {
                     mainCommands.identifyCommand(request);
+                }
+            }
+        }
+
+        public static void PostsPage(bool whithPromt = false)
+        {
+            customWrite.writeLine("Вы зашли на главную страницу с постами !consoleWEB");
+            if (PostsPageCalls == 0)
+            {
+                customWrite.writeLine("/help для описания работы с страницой, /out чтобы выйти");
+            }
+            PostsPageCalls++;
+            while (true) 
+            {
+                string request = Console.ReadLine();
+                if (request == "" || request[0] != '/')
+                {
+                    customWrite.writeLine("Используйте '/' для оринтирования в программе!");
+                }
+                else
+                {
+                    postsPageCommands.identifyCommand(request);
                 }
             }
         }
